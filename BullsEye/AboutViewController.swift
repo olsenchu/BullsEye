0363bbf60398c66756f10481902caea060df77b3
olsenchu <jjavier7@hawkmail.hccfl.edu>
2022-01-16 20:27:16 -0500

    Initial commit

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by jeni on 1/16/22.
+//
+
+import UIKit
+import WebKit
+
+class AboutViewController: UIViewController {
+    @IBOutlet var webView: WKWebView!
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let url = Bundle.main.url(
+            forResource: "BullsEye", withExtension: "html") {
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
+        
+    }
+    
+}
